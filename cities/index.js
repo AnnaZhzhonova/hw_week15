531e7ae3fc374df6c786786b97632300731b089a
annzhzho@gmail.com <annzhzho@gmail.com>
2023-06-15 19:36:09 +0300

    add cities task

---
+++ cities/index.js
@@ -0,0 +1,35 @@
+const citiesList = document.getElementById("cities-list");
+
+const cities = ["Москва", "Санкт-Петербург", "Нью-Йорк", "Токио"];
+const temperatures = [];
+
+for (let city of cities) {
+  const inputTemp = prompt(`Введите температуру для города ${city}`);
+  temperatures.push(inputTemp);
+  const cityItem = document.createElement("li");
+  cityItem.textContent = `Температура в городе ${city}: ${inputTemp} °С`;
+  citiesList.append(cityItem);
+}
+
+let minimum = -Infinity;
+let maximum = Infinity;
+
+for (let i = 0; i < temperatures.length; i++) {
+  const temp = temperatures[i];
+
+  if (temp < minimum) {
+    minimum = temp;
+  }
+
+  if (temp > maximum) {
+    maximum = temp;
+  }
+}
+console.log(minimum);
+
+const minTemp = document.createElement("p");
+const maxTemp = document.createElement("p");
+minTemp.textContent = `Минимальная температура: ${minimum} °С`;
+maxTemp.textContent = `Маскимальная температура: ${maximum} °С`;
+citiesList.after(minTemp);
+citiesList.after(maxTemp);
